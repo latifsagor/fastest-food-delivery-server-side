3041658112cc871dd8375829cfb6c58094f4c6ff
latifsagor <latif.sagor1042@gmail.com>
2021-10-31 07:42:50 +0600

    update

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tigkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
+console.log(uri)
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+
+console.log('All route should be work')
 
 async function run() {
   try {
