765e1d597a041c6b46b34fea8cf242f89e45b0d5
latifsagor <latif.sagor1042@gmail.com>
2021-10-31 08:20:45 +0600

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const cors = require('cors')
 const app = express()
-const port = process.env.Port || 5000
+const port = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
